4475077b8c563a44d5cdfc9bae1e58a432df7c0e
Tom Callister <thomas.a.callister@gmail.com>
2022-03-23 14:18:44 -0400

    More likelihoods

--- code/numpyroCode/likelihoods.py
+++ code/numpyroCode/likelihoods.py
@@ -4,14 +4,35 @@ import jax.numpy as jnp
 from jax.scipy.special import erf
 from jax import vmap
 
-def asym(x):
-    return -jnp.exp(-x**2)/jnp.sqrt(np.pi)/x*(1.-1./(2.*x**2))
-
 def truncatedNormal(samples,mu,sigma,lowCutoff,highCutoff):
+
+    """
+    Jax-enabled truncated normal distribution
+    
+    Parameters
+    ----------
+    samples : `jax.numpy.array` or float
+        Locations at which to evaluate probability density
+    mu : float
+        Mean of truncated normal
+    sigma : float
+        Standard deviation of truncated normal
+    lowCutoff : float
+        Lower truncation bound
+    highCutoff : float
+        Upper truncation bound
+
+    Returns
+    -------
+    ps : jax.numpy.array or floaot
+        Probability density at the locations of `samples`
+    """
+
     a = (lowCutoff-mu)/jnp.sqrt(2*sigma**2)
     b = (highCutoff-mu)/jnp.sqrt(2*sigma**2)
     norm = jnp.sqrt(sigma**2*np.pi/2)*(-erf(a) + erf(b))
-    return jnp.exp(-(samples-mu)**2/(2.*sigma**2))/norm
+    ps = jnp.exp(-(samples-mu)**2/(2.*sigma**2))/norm
+    return ps
 
 def gaussian(sampleDict,injectionDict,mMin):
 
@@ -211,10 +232,12 @@ def doubleGaussian(sampleDict,injectionDict,mMin):
     # Tally log-likelihoods across our catalog
     numpyro.factor("logp",jnp.sum(log_ps))
 
-def gaussianSpike_MonteCarloAvg(sampleDict,injectionDict,mMin,sig_eps):
+def gaussianSpike(sampleDict,injectionDict,mMin,sig_eps):
 
     """
-    Implementation of a Gaussian and zero-spin spike effective spin distribution for inference within `numpyro`
+    Implementation of a Gaussian and zero-spin spike effective spin distribution for inference within `numpyro`.
+    As this implementation makes use of KDEs over posterior samples and found injections, the "spike width"
+    parameter `sig_eps` can be zero.
 
     Parameters
     ----------
@@ -225,7 +248,120 @@ def gaussianSpike_MonteCarloAvg(sampleDict,injectionDict,mMin,sig_eps):
     mMin : float
         Minimum black hole mass
     sig_eps : float
-        Width of "spike" mixture component. We generally choose `sig_eps=0`
+        Width of "spike" mixture component.
+    """
+    
+    # Sample our hyperparameters
+    # bq: Power-law index on the conditional secondary mass distribution p(m2|m1)
+    # mu: Mean of the chi-effective distribution
+    # logsig_chi: Log10 of the chi-effective distribution's standard deviation
+    bq = numpyro.sample("bq",dist.Normal(0,3))
+    mu = numpyro.sample("mu_chi",dist.Uniform(-1,1))
+    logsig_chi = numpyro.sample("logsig_chi",dist.Uniform(-1.5,0))
+    sig = 10.**logsig_chi
+
+    # Also sample the mixture fraction governing the number of events in the bulk Gaussian.
+    # In order to faciliate more efficient sampling, we explicitly sample logit(zeta) rather than zeta directly.
+    # This is then converted to zeta, and an appropriate term added to our log-likelihood to ensure
+    # a uniform prior on zeta
+    logit_zeta_bulk = numpyro.sample("logit_zeta_bulk",dist.Normal(0,2))
+    zeta_bulk = jnp.exp(logit_zeta_bulk)/(1.+jnp.exp(logit_zeta_bulk)) 
+    numpyro.deterministic("zeta_bulk",zeta_bulk)
+    zeta_bulk_logprior = -0.5*logit_zeta_bulk**2/2**2 + jnp.log(1./zeta_bulk + 1./(1-zeta_bulk))
+    numpyro.factor("uniform_zeta_bulk_prior",-zeta_bulk_logprior)
+
+    # Read out found injections
+    # Note that `pop_reweight` is the inverse of the draw weights for each event
+    Xeff_det = injectionDict['Xeff']
+    m1_det = injectionDict['m1']
+    m2_det = injectionDict['m2']
+    pop_reweight = injectionDict['pop_reweight']
+
+    # Probability of each injection under the proposed population
+    # See discussion of KDE likelihood methods in paper text
+    sig_kde = 0.5*jnp.std(Xeff_det)*Xeff_det.size**(-1./5.)
+    bulk_denom = jnp.sqrt(2.*jnp.pi*(sig_kde**2+sig**2))*(erf((1.-mu)/jnp.sqrt(2.*sig**2)) + erf((1.+mu)/jnp.sqrt(2.*sig**2)))
+    spike_denom = jnp.sqrt(2.*jnp.pi*(sig_kde**2+sig_eps**2))*(erf(1./jnp.sqrt(2.*sig_eps**2)) + erf(1./jnp.sqrt(2.*sig_eps**2)))
+    #spike_denom = (2.*jnp.sqrt(2.*jnp.pi*(sig_kde**2+sig_eps**2)))*erf(1./jnp.sqrt(2.*sig_eps**2))
+    bulk_kde_integrals = (erf((sig_kde**2*(1.+mu)+sig**2*(1.+Xeff_det))/jnp.sqrt(2.*sig_kde**2*sig**2*(sig_kde**2+sig**2)))\
+                        - erf((sig_kde**2*(mu-1.)+sig**2*(Xeff_det-1.))/jnp.sqrt(2.*sig_kde**2*sig**2*(sig_kde**2+sig**2))))\
+                    *jnp.exp(-(Xeff_det-mu)**2/(2.*(sig_kde**2+sig**2)))/bulk_denom
+    spike_kde_integrals = (erf((sig_kde**2+sig_eps**2*(1.+Xeff_det))/jnp.sqrt(2.*sig_kde**2*sig_eps**2*(sig_kde**2+sig_eps**2)))\
+                        - erf((sig_kde**2*(-1.)+sig_eps**2*(Xeff_det-1.))/jnp.sqrt(2.*sig_kde**2*sig_eps**2*(sig_kde**2+sig_eps**2))))\
+                    *jnp.exp(-Xeff_det**2/(2.*(sig_kde**2+sig_eps**2)))/spike_denom
+    
+    # Form ratio of proposed population weights over draw weights for each found injection
+    p_chi_det = zeta_bulk*bulk_kde_integrals + (1.-zeta_bulk)*spike_kde_integrals
+    p_m2_det = (1.+bq)*m2_det**bq/(m1_det**(1.+bq) - mMin**(1.+bq))
+    p_m2_det = jnp.where(m2_det<mMin,0.,p_m2_det)
+    xi_weights = p_chi_det*p_m2_det*pop_reweight
+    
+    # As a fit diagnostic, compute effective number of injections
+    nEff_inj = jnp.sum(xi_weights)**2/jnp.sum(xi_weights**2)
+    numpyro.deterministic("nEff_inj_per_event",nEff_inj/66.)
+
+    # Compute net detection efficiency and add to log-likelihood
+    xi = jnp.sum(xi_weights)
+    numpyro.factor("xi",-66*jnp.log(xi))
+    
+    # This function defines the per-event log-likelihood
+    # m1_sample: Primary mass posterior samples
+    # m2_sample: Secondary mass posterior samples
+    # Xeff_sample: Effective spin posterior samples
+    # weights: Factors that convert to the desired m1/redshift distribution and divide out the m2 and spin prior
+    def logp(m1_sample,m2_sample,Xeff_sample,weights):
+        
+        # KDE likelihood; see paper text
+        sig_kde = 0.5*jnp.std(Xeff_sample)*Xeff_sample.size**(-1./5.)
+        bulk_denom = jnp.sqrt(2.*jnp.pi*(sig_kde**2+sig**2))*(erf((1.-mu)/jnp.sqrt(2.*sig**2)) + erf((1.+mu)/jnp.sqrt(2.*sig**2)))
+        spike_denom = jnp.sqrt(2.*jnp.pi*(sig_kde**2+sig_eps**2))*(erf(1./jnp.sqrt(2.*sig_eps**2)) + erf(1./jnp.sqrt(2.*sig_eps**2)))
+        bulk_kde_integrals = (erf((sig_kde**2*(1.+mu)+sig**2*(1.+Xeff_sample))/jnp.sqrt(2.*sig_kde**2*sig**2*(sig_kde**2+sig**2)))\
+                            - erf((sig_kde**2*(mu-1.)+sig**2*(Xeff_sample-1.))/jnp.sqrt(2.*sig_kde**2*sig**2*(sig_kde**2+sig**2))))\
+                        *jnp.exp(-(Xeff_sample-mu)**2/(2.*(sig_kde**2+sig**2)))/bulk_denom
+        spike_kde_integrals = (erf((sig_kde**2+sig_eps**2*(1.+Xeff_sample))/jnp.sqrt(2.*sig_kde**2*sig_eps**2*(sig_kde**2+sig_eps**2)))\
+                            - erf((sig_kde**2*(-1.)+sig_eps**2*(Xeff_sample-1.))/jnp.sqrt(2.*sig_kde**2*sig_eps**2*(sig_kde**2+sig_eps**2))))\
+                        *jnp.exp(-Xeff_sample**2/(2.*(sig_kde**2+sig_eps**2)))/spike_denom
+    
+        # Form total population prior
+        p_chi = zeta_bulk*bulk_kde_integrals + (1.-zeta_bulk)*spike_kde_integrals
+        p_m2 = (1.+bq)*m2_sample**bq/(m1_sample**(1.+bq) - mMin**(1.+bq))
+        p_m2 = jnp.where(m2_sample<mMin,0.,p_m2)
+        mc_weights = p_chi*p_m2*weights
+        
+        # Compute effective number of samples and return log-likelihood
+        n_eff = jnp.sum(mc_weights)**2/jnp.sum(mc_weights**2)     
+        return jnp.log(jnp.mean(mc_weights)),n_eff
+    
+    # Map the log-likelihood function over each event in our catalog
+    log_ps,n_effs = vmap(logp)(
+                        jnp.array([sampleDict[k]['m1'] for k in sampleDict]),
+                        jnp.array([sampleDict[k]['m2'] for k in sampleDict]),
+                        jnp.array([sampleDict[k]['Xeff'] for k in sampleDict]),
+                        jnp.array([sampleDict[k]['weights_over_priors'] for k in sampleDict]))
+        
+    # As a diagnostic, save minimum number of effective samples across all events
+    numpyro.deterministic('min_log_neff',jnp.min(jnp.log10(n_effs)))
+
+    # Tally log-likelihoods across our catalog
+    numpyro.factor("logp",jnp.sum(log_ps))
+
+def gaussianSpike_MonteCarloAvg(sampleDict,injectionDict,mMin,sig_eps):
+
+    """
+    Implementation of a Gaussian and zero-spin spike effective spin distribution for inference within `numpyro`.
+    Relies on standard Monte Carlo averaging of posterior samples and found injections, hence the "spike width"
+    parameter `sig_eps` must be non-zero.
+
+    Parameters
+    ----------
+    sampleDict : dict
+        Precomputed dictionary containing posterior samples for each event in our catalog
+    injectionDict : dict
+        Precomputed dictionary containing successfully recovered injections
+    mMin : float
+        Minimum black hole mass
+    sig_eps : float
+        Width of "spike" mixture component.
     """
     
     # Sample our hyperparameters
--- docs/build/html/_sources/test.rst.txt
+++ docs/build/html/_sources/test.rst.txt
@@ -2,3 +2,9 @@ Test
 ====
 
 .. autofunction:: numpyroCode.likelihoods.gaussian
+
+.. autofunction:: numpyroCode.likelihoods.doubleGaussian
+
+.. autofunction:: numpyroCode.likelihoods.gaussianSpike
+
+.. autofunction:: numpyroCode.likelihoods.gaussianSpike_MonteCarloAvg
